487efd4a242c56a2cd1f52ba775e7fe5efa1fc0d
Peter Luknár <github.com@vyprazanysyr.sk>
2017-02-18 21:50:04 +0100

    edits

--- twilio_connector.py
+++ twilio_connector.py
@@ -3,34 +3,50 @@
 from twilio.rest import Client
 import conf
 import argparse
+import datetime
 import sys
 
 c = conf.Conf
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--to", action='store', help="Number in international format to call")
-parser.add_argument("-c", "--configlist", action="store_true", help="List configuration vars")
-parser.add_argument("-d", "--debug", action="store_true", help="Print configuration vars before placing call")
 args = parser.parse_args()
 
 class Zabbix_connector:
+  global args
+
   def connectToApi(self):
       return Client(c.config['account_sid'], c.config['auth_token'])
 
-  def makeCall(self):
-    call = self.connectToApi().calls.create(to=c.config['number_from'],
-                               from_= args.to if args.to else c.config['number_from'],
-                               url=c.config['twiml_file_url'])
-    print(call.sid)
+  def numberTo(self):
+    return args.to if args.to else c.config['default_number_to']
 
-  def getConfig(self):
-    for key, value in c.config.iteritems():
-      if not value:
-        value = "_EMPTY_"
-      print("{}:\t{}".format(key, value))
+  def numberFrom(self):
+    return c.config['number_from']
+
+  def makeCall(self):
+    call = self.connectToApi().calls.create(
+      to=self.numberTo(),
+      from_=c.config['number_from'],
+      url=c.config['twiml_file_url'])
+    return(call)
+
+  def logCall(self, call):
+    log = {'from': call.from_,
+           'to': call.to,
+           'sid': call.sid,
+           'timestamp': datetime.date.today().strftime("%x %X"),
+           'status': call.status,
+           }
+
+    f = open(c.config['log_file'], 'aw')
+    f.write(str(log) + "\n")
+    f.close()
 
 z = Zabbix_connector()
-if args.configlist:
-  z.getConfig()
-  sys.exit(0)
 
-z.makeCall()
+# make actual call
+call = z.makeCall()
+
+# if logging is on, write to log
+z.logCall(call)
+sys.exit(0)
