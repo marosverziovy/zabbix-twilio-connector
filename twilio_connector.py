3686a137bc435cf8d6ff6c30695bcb2b0b5295d2
Peter Luknár <github.com@vyprazanysyr.sk>
2017-02-20 07:26:51 +0100

    fix indent

--- twilio_connector.py
+++ twilio_connector.py
@@ -8,49 +8,51 @@ import sys
 
 class Zabbix_connector:
 
-  global args
+    global args
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument("to", action='store', help="Number in international format to call")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("to", action='store', help="Number in international format to call")
+    args = parser.parse_args()
 
+    def connectToApi(self):
+        return Client(c.config['account_sid'], c.config['auth_token'])
 
-  def connectToApi(self):
-      return Client(c.config['account_sid'], c.config['auth_token'])
+    def numberTo(self):
+        if args.to:
+            return args.to
+        else:
+            return c.config['default_number_to']
 
-  def numberTo(self):
-    return args.to if args.to else c.config['default_number_to']
+    def numberFrom(self):
+        return c.config['number_from']
 
-  def numberFrom(self):
-    return c.config['number_from']
+    def makeCall(self):
+        call = self.connectToApi().calls.create(
+            to=self.numberTo(),
+            from_=self.numberFrom(),
+            url=c.config['twiml_file_url'])
+        return(call)
 
-  def makeCall(self):
-    call = self.connectToApi().calls.create(
-      to=self.numberTo(),
-      from_=self.numberFrom(),
-      url=c.config['twiml_file_url'])
-    return(call)
+    def logCall(self, call):
+        log = {'from': call.from_,
+               'to': call.to,
+               'sid': call.sid,
+               'timestamp': datetime.date.today().strftime("%x %X"),
+               'status': call.status,
+               }
 
-  def logCall(self, call):
-    log = {'from': call.from_,
-           'to': call.to,
-           'sid': call.sid,
-           'timestamp': datetime.date.today().strftime("%x %X"),
-           'status': call.status,
-           }
-
-    f = open(c.config['log_file'], 'aw')
-    f.write(str(log) + "\n")
-    f.close()
+        f = open(c.config['log_file'], 'aw')
+        f.write(str(log) + "\n")
+        f.close()
 
 if __name__ == "__main__":
-  z = Zabbix_connector()
+    z = Zabbix_connector()
 
-  # make actual call
-  call = z.makeCall()
+    # make actual call
+    call = z.makeCall()
 
-  # if logging is on, write to log
-  if c.config['logging_enabled'] == True:
-    z.logCall(call)
+    # if logging is on, write to log
+    if c.config['logging_enabled'] == True:
+        z.logCall(call)
 
-  sys.exit(0)
+    sys.exit(0)
